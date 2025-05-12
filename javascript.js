f45763783606c90362a6a4e2490d2b8dbd9ac8bd
Satyajit <satyajitmanna35@gmail.com>
2025-05-12 16:16:35 +0530

    fix the bug of animation js.

--- javascript.js
+++ javascript.js
@@ -1,7 +1,6 @@
     //    Setup and start animation! 
     
       var typed = new Typed('#element', {
-        
             strings: ['Android Developer.', 'Web Developer.'],
             typeSpeed: 50,
             backSpeed: 30,
