affbc0ec701e5f8abf1ccf1827daf602bec426fd
Satyajit <satyajitmanna35@gmail.com>
2025-05-12 16:09:26 +0530

    add hamburger features into the nav bar

---
+++ javascript.js
@@ -0,0 +1,14 @@
+    //    Setup and start animation! 
+    
+      var typed = new Typed('#element', {
+        
+            strings: ['Android Developer.', 'Web Developer.'],
+            typeSpeed: 50,
+            backSpeed: 30,
+            loop: true
+        });
+    
+    function toggleMenu() {
+            const navLinks = document.getElementById("nav-links");
+            navLinks.classList.toggle("show");
+        }
